a7306815e0083b424bf12c91fe5e4fc5f0b14090
LeoBueno15 <leonardo.d.bueno.23@dartmouth.edu>
2022-10-20 20:03:39 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Leonardo Bueno</h1>
+        <p class="paragraph-font">
+            Hello, this is my first html and css project. I made this sample website to test html and css. This website is all about me?
+            So... here are some things about me.
+        </p>
+        <h3>&#128197; 2023</h3>
+        <h3>&#127874; May 29 2001</h3>
+        <h3>&#127968; Los Angeles</h3>
+        <h4>Here are some facts about me:</h4>
+        <ul>
+            <li>My favorite snack is pumpkin seeds</li>
+            <li>My favorite animal is the octopus</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
